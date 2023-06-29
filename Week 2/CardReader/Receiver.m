70a9827cd4eb9bfdfd880cd72686f64738be6ddb
Adam Kamrath <akamrath2@unl.edu>
2023-06-29 14:01:52 -0500

    Updated notes for week2 and created bb files for different reader commands

--- Week 2/CardReader/Receiver.m
+++ Week 2/CardReader/Receiver.m
@@ -1,34 +1,29 @@
 center_frequency = 13560000;
-sampling_rate = 156250;
+sampling_rate = 200000;
 decimation_factor = round(100000000/sampling_rate);
-
-pause(3);
 
 rx = comm.SDRuReceiver(...
                     Platform = "N200/N210/USRP2", ...
-                    IPAddress = '192.168.10.3', ...
+                    IPAddress = '192.168.10.2', ...
                     OutputDataType = "double", ...
                     DecimationFactor = decimation_factor, ...
                     Gain = 0, ...
                     CenterFrequency = center_frequency, ...
-                    SamplesPerFrame = 156250*5);
+                    SamplesPerFrame = 200000);
 
-
-
-
-
-timeScope = timescope(TimeSpanSource = "Property",...
-                      TimeSpan = 5,SampleRate = sampling_rate);
+timeScope = timescope(TimeSpanSource = "auto",...
+                      TimeSpan = 5,SampleRate = sampling_rate, ...
+                      Position=[20,200,800,500]);
 timeScope.YLimits = [-.6,.6];
 
 disp("Reception Started");
 frameduration = (rx.SamplesPerFrame)/(sampling_rate); 
 time = 0; 
 while time < 5
+  disp(time);
   data = rx();
   timeScope(data)
   time = time + frameduration;
-  disp(time);
 end
 
 disp("Reception Stopped");
