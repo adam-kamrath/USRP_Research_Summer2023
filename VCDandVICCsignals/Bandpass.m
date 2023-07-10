edbca285ea3a98bdbf49c6123372b1808704729f
Adam Kamrath <akamrath2@unl.edu>
2023-07-10 13:18:00 -0500

    Updated VCDanVICCSignals

--- VCDandVICCsignals/Bandpass.m
+++ VCDandVICCsignals/Bandpass.m
@@ -1,9 +1,10 @@
-bandpassSpecs = fdesign.bandpass('N,Fst1,Fp1,Fp2,Fst2,C',100,2e5,4e5,5e5,7e5,10e6);
+bandpassSpecs = fdesign.bandpass('N,Fst1,Fp1,Fp2,Fst2,C',300,4e5,4.25e5,4.5e5,4.75e5,2e6);
 bandpassSpecs.Stopband1Constrained = true;
 bandpassSpecs.Astop1 = 60;
 bandpassSpecs.Stopband2Constrained = true;
 bandpassSpecs.Astop2 = 60;
 
-bandpassFilter = design(bandpassSpecs, 'Systemobject', true);
-fvtool(bandpassFilter)
-measure(bandpassFilter)
+BandpassFilter = design(bandpassSpecs, 'Systemobject', true);
+fvtool(BandpassFilter)
+measure(BandpassFilter)
+release(BandpassFilter);
--- VCDandVICCsignals/Reader.m
+++ VCDandVICCsignals/Reader.m
@@ -1,31 +1,43 @@
-sampling_rate = 10000000;
+sampling_rate = 2000000;
 decimation_factor = 100000000/sampling_rate;
 
 
-reader = comm.BasebandFileReader('higher_sample_card.bb', SamplesPerFrame=inf);
+reader = comm.BasebandFileReader('system_information.bb', SamplesPerFrame=inf);
 %Create the time scope for the unfiltered data
 dataTimeScope = timescope(TimeSpanSource = "auto",...
-                      TimeSpan = 5,SampleRate = sampling_rate, ...
+                      TimeSpan = 10,SampleRate = sampling_rate, ...
                       Position=[20,100,800,500], ...
                       YLimits=[-.6,.6]);
 %Create the time scope for the highpass filtered data
 highpassTimeScope = timescope(TimeSpanSource = "auto",...
-                      TimeSpan = 5,SampleRate = sampling_rate, ...
+                      TimeSpan = 10,SampleRate = sampling_rate, ...
                       Position=[1000,100,800,500], ...
                       YLimits=[-.6,.6]);
 
+bandpassTimeScope = timescope(TimeSpanSource = "auto",...
+                      TimeSpan = 10,SampleRate = sampling_rate, ...
+                      Position=[1000,100,800,500], ...
+                      YLimits=[-.6,.6]);
+
+spectrum = spectrumAnalyzer(Samplerate=sampling_rate, ...
+    FrequencySpan="span-and-center-frequency", Span=1e6);
 %Get the data from the file
 data = reader();
 
 %Put data through highpass filter
-highpass_data = FIRHPF(data);
+highpass_data = HighpassFilter(data);
+bandpass_data = BandpassFilter(highpass_data);
+spectrum(bandpass_data);
 
 %Plot data
 dataTimeScope(data);
 highpassTimeScope(highpass_data);
+bandpassTimeScope(bandpass_data);
 
 %release instances
 release(dataTimeScope);
 release(highpassTimeScope);
-release(FIRHPF);
+release(HighpassFilter);
+release(bandpassTimeScope);
+release(BandpassFilter);
 release(reader);
