7906d3e5a5af51a4aa143e2e8bf7b9d96b69e5ad
Adam Kamrath <akamrath2@unl.edu>
2023-07-14 12:05:00 -0500

    Updated decodeCardData

--- VCDandVICCsignals/decodeCardData.m
+++ VCDandVICCsignals/decodeCardData.m
@@ -2,7 +2,7 @@ function decodeCardData(data)
     %Starting parameters
     binary_sequence = '';
     %5753340
-    current_sample = 1000;
+    current_sample = 5752497;
     SOFfound = false;
     EOFfound = false;
     findSOF(data, current_sample);
@@ -14,38 +14,51 @@ function findSOF(data, current_sample)
     sample_time = 1/sampling_rate;
     SOFfound = false;
     current_sample = findNextOne(data, current_sample);
-    [pulses, current_sample] = getPulses(data, current_sample);
     while SOFfound == false
-        disp(current_sample);
         current_sample = findNextOne(data, current_sample);
         [pulses, current_sample] = getPulses(data, current_sample);
         if pulses ~= 24
             continue;
         end
+        one_sample = findNextOne(data, current_sample);
+        zero_time = (one_sample - current_sample) * sample_time;
+        current_sample = one_sample;
+        if (16e-06 < zero_time) && (zero_time < 20e-06)
+            continue;
+        end
+        [pulses, current_sample] = getPulses(data, current_sample);
+        if pulses == 8
+            continue;
+        end
         SOFfound = true;
-        disp(pulses);
-        disp(current_sample * sample_time);
     end
 end
 
 function [pulses, current_sample] = getPulses(data, current_sample)
     %Starting parameters
     sampling_rate = 2000000;
-    sample_time = 1/sampling_rate;
-    pulses = 0;
+    sample_time = 1/sampling_rate;  
+    pulses = 1;
     zero_time = 0;
     %Gets the first zero after the frame starts
     current_sample = findNextZero(data, current_sample);
     zero_sample = current_sample;
     %Finds all pulses
-    while zero_time < 3e-06
-        current_sample = findNextOne(data, current_sample);
-        one_sample = current_sample;
+    while true
+        one_sample = findNextOne(data, current_sample);
+        current_sample = one_sample;
         zero_time = (one_sample - zero_sample) * sample_time;
-        pulses = pulses + 1;
-        current_sample = findNextZero(data, current_sample);
-        zero_sample = current_sample;
+        if zero_time < 3e-06
+            pulses = pulses + 1;
+            current_sample = findNextZero(data, current_sample);
+            zero_sample = current_sample;
+            continue;
+        end
+        disp(pulses);
+        disp(current_sample * sample_time);
+        return;
     end
+    
 end
 
 %This will find start of the next pulse
